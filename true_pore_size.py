6c97a2810d5455b18fd48fa048835451bcc31b73
Moritz Marquardt <moritz.g.marquardt@gmail.com>
2024-02-11 11:32:42 +0100

    working multi time analysis filtering error removed

--- true_pore_size.py
+++ true_pore_size.py
@@ -6,27 +6,18 @@ import time
 
 start = time.time()
 
-# one timeframe in the gro file
-'''gro_file_path = '/bigpool/users/ac130484/project/hex_box_hex_dod/hex_18_2_3_n/simulation/20fs.gro'
-u = mda.Universe(gro_file_path)
-c_atoms = u.select_atoms('resname C')
-hex_atoms = u.select_atoms('resname HEX')
-dod_atoms = u.select_atoms('resname DOD')
-c_atomic_positions = c_atoms.positions
-dod_atomic_positions = dod_atoms.positions
-hex_atomic_positions = hex_atoms.positions'''
-
 # several time frames in the xtc file and tpr as topology file
-# u = mda.Universe('/bigpool/users/ac130484/project/hex_box_hex_dod/hex_18_2_3_n/simulation/20fs.tpr','/bigpool/users/ac130484/project/hex_box_hex_dod/hex_18_2_3_n/simulation/20fs.gro')
+u = mda.Universe('/bigpool/users/ac130484/project/hex_box_hex_dod/hex_18_2_3_n/simulation/20fs.tpr','/bigpool/users/ac130484/project/hex_box_hex_dod/hex_18_2_3_n/simulation/20fs.gro')
 # u = mda.Universe('/bigpool/users/st166545/TPS/240130kri_new_sim/traj_firstframe.gro', '/bigpool/users/st166545/TPS/240130kri_new_sim/traj.xtc')
 # u = mda.Universe('/bigpool/data/projects/Carbon_pores_Sofia/kri_with_vacuum_test/hex_18_3_2_n_vacuum/simulation_1/topol.tpr', '/bigpool/users/st166545/TPS/kri_with_vacuum_test_skip10.xtc')
 print(u)
 
 
 c_atoms = u.select_atoms('resname C')
-print(c_atoms)
 hex_atoms = u.select_atoms('resname HEX')
 dod_atoms = u.select_atoms('resname DOD')
+print(c_atoms)
+
 c_atomic_positions = np.zeros((u.trajectory.n_frames,len(c_atoms),3))
 dod_atomic_positions = np.zeros((u.trajectory.n_frames,len(dod_atoms),3))
 hex_atomic_positions = np.zeros((u.trajectory.n_frames,len(hex_atoms),3))
@@ -46,7 +37,7 @@ for ts in u.trajectory:
 print("pos aquire done after: " + str(time.time()-start))
 
 # Set constraints on y and z directions
-y_min, y_max = 29, 31  # Replace with your desired range
+y_min, y_max = 27, 33  # Replace with your desired range
 z_min, z_max = 250, 400  # Replace with your desired range
 
 # optional histogrmas to detect the bounds
@@ -63,6 +54,7 @@ c_filtered_indices = np.where(
     (c_atomic_positions[:,:, 1] >= y_min) & (c_atomic_positions[:,:, 1] <= y_max) &
     (c_atomic_positions[:,:, 2] >= z_min) & (c_atomic_positions[:,:, 2] <= z_max)
 )
+print(c_filtered_indices)
 
 dod_filtered_indices = np.where(
     (dod_atomic_positions[:,:, 1] >= y_min) & (dod_atomic_positions[:,:, 1] <= y_max) &
@@ -74,9 +66,9 @@ hex_filtered_indices = np.where(
     (hex_atomic_positions[:,:, 2] >= z_min) & (hex_atomic_positions[:,:, 2] <= z_max)
 )
 
-c_filtered_x_positions = c_atomic_positions[:,c_filtered_indices, 0].flatten()
-dod_filtered_x_positions = dod_atomic_positions[:,dod_filtered_indices, 0].flatten()
-hex_filtered_x_positions = hex_atomic_positions[:,hex_filtered_indices, 0].flatten()
+c_filtered_x_positions = c_atomic_positions[c_filtered_indices[0],c_filtered_indices[1], 0].flatten()
+dod_filtered_x_positions = dod_atomic_positions[dod_filtered_indices[0],dod_filtered_indices[1], 0].flatten()
+hex_filtered_x_positions = hex_atomic_positions[hex_filtered_indices[0],hex_filtered_indices[1], 0].flatten()
 
 print("filtering done after: " + str(time.time()-start))
 
@@ -88,45 +80,9 @@ dod_hex_hist, dod_hex_bin_edges= np.histogram(np.append(dod_filtered_x_positions
 
 print("hist calc done after: " + str(time.time()-start))
 
-# chat gpt
-def largest_interval_below_threshold(counts, bin_edges, threshold):
-
-    largest_interval = None
-    current_interval = None
-    for i in range(len(bin_edges) - 1):
-        bin_value = counts[i]
-        if bin_value < threshold:
-            if current_interval is None:
-                current_interval = [bin_edges[i], bin_edges[i]]
-            else:
-                current_interval[1] = bin_edges[i]
-        else:
-            if current_interval is not None:
-                if largest_interval is None or (current_interval[1] - current_interval[0] > largest_interval[1] - largest_interval[0]):
-                    largest_interval = current_interval
-                current_interval = None
-
-    if current_interval is not None:
-        if largest_interval is None or (current_interval[1] - current_interval[0] > largest_interval[1] - largest_interval[0]):
-            largest_interval = current_interval
-
-    return largest_interval
-
-#largest_interval_c = largest_interval_below_threshold(c_hist,c_bin_edges,3)
-
-
-# optimisation problem
-'''largest_interval_c = [0,1]
-interval = spo.minimize'''
-
-# print(f"Largest interval with values below 30: {largest_interval_c}")
-# print(f"True pore size: {largest_interval_c[1]-largest_interval_c[0]}")
-
 
 # Plot all histograms in one plot
 plt.figure()
-# plt.axvline(x=largest_interval_c[0], color='red', linestyle='--')
-# plt.axvline(x=largest_interval_c[1], color='red', linestyle='--')
 plt.plot(c_bin_edges[:-1], c_hist, label='C', linestyle='-', marker='o', markersize=3)
 plt.plot(dod_hex_bin_edges[:-1], dod_hex_hist, label='DOD & HEX', linestyle='-', marker='o', markersize=3)
 
@@ -136,8 +92,4 @@ plt.title('Histogram Line Plot along the X-axis with Y and Z constraints for Res
 plt.grid(True)
 plt.legend()
 
-
-# Show the plot for 2 seconds
 plt.show()
-time.sleep(2)
-plt.close()
