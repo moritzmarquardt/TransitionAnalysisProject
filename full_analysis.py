598034cac0bee54d01132b7fe35a5341b38c1cdb
Moritz Marquardt <moritz.g.marquardt@gmail.com>
2024-06-17 15:24:09 +0200

    DOC

--- full_analysis.py
+++ full_analysis.py
@@ -1,4 +1,4 @@
-import MembraneAnalysisToolbox.TransitionPathAnalysis as mat
+import MembraneAnalysisToolbox.TransitionPathAnalysis as TPA
 import matplotlib.pyplot as plt
 
 '''
@@ -7,27 +7,26 @@ this file is for testing and development purposes only
 
 path = "/bigpool/users/ac130484/project/finished_sim/hex/poresize/3nm_NVT/simulation_1/"
 
-Analysis2nm_1 = mat.TransitionPathAnalysis( 
+# STEP 1: initialise the Data into the class
+Analysis2nm_1 = TPA.TransitionPathAnalysis( 
     topology_file = path + 'topol.tpr', 
     trajectory_file = path + 'traj.xtc',
     verbose = True
 )
 L = 180
+
+# STEP 2: Find z_lower and validate by eye
 z_lower = Analysis2nm_1.find_z_lower_hexstruc(mem_selector = "resname C", L=L)
 print("z_lower: " + str(z_lower))
 Analysis2nm_1.inspect(["resname HEX and name C1"], z_lower, L)
-Analysis2nm_1.inspect(["resname C"], z_lower, L)
 
+# STEP 3: analyse passage times and calculate diffusion coefficient
 # Hex analysis
 print("Hex analysis")
 ffs, ffe, indizes = Analysis2nm_1.calc_passagetimes(["resname HEX and name C1"], z_lower, L)
 print("passages: " + str(len(ffs)))
 D = Analysis2nm_1.calc_diffusion(list(ffe-ffs), L, T = 296)
 print("Diffusioncoefficient: " + str(D))
-plt.show()
-# Analysis2nm_1.plot_passagetimedist("resname HEX and name C1")
-# Analysis2nm_1.plot_rand3dtrajs("resname HEX and name C1")
-# Analysis2nm_1.plot_startpoints("resname HEX and name C1")
 plt.show()
 
 # Dod analysis
@@ -36,8 +35,4 @@ ffs, ffe, indizes = Analysis2nm_1.calc_passagetimes(["resname DOD and name C2"],
 print("passages: " + str(len(ffs)))
 D = Analysis2nm_1.calc_diffusion(list(ffe-ffs), L, T = 296)
 print("Diffusioncoefficient: " + str(D))
-plt.show()
-# Analysis2nm_1.plot_passagetimedist("resname HEX and name C1")
-# Analysis2nm_1.plot_rand3dtrajs("resname HEX and name C1")
-# Analysis2nm_1.plot_startpoints("resname HEX and name C1")
 plt.show()
