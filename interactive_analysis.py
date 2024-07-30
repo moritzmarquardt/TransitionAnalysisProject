fc08a0371ffa8db43b925df01730bc0806662205
Moritz Marquardt <moritz.g.marquardt@gmail.com>
2024-07-30 11:26:42 +0200

    fix: adapt to new package structure and add feature to enter cubic or hex structure

--- interactive_analysis.py
+++ interactive_analysis.py
@@ -2,9 +2,11 @@ import json
 
 import matplotlib.pyplot as plt
 from MembraneAnalysisToolbox.DiffusionAnalysis import DiffusionAnalysis
+from MembraneAnalysisToolbox.MembraneStructures import CubicMembrane, HexagonalMembrane
 
 path = input("Enter the path to the simulation: ")
-print("Path: " + path)
+print("Path: " + path + "\n")
+cubic_or_hex = input("Enter the type of membrane (cubic (C) or hexagonal (H)): ")
 
 
 def analyse_resname(selector: str, short: str):
@@ -12,14 +14,35 @@ def analyse_resname(selector: str, short: str):
 
     DA.calc_passagetimes(selector)
     print(f"\t{short}-passages: " + str(len(DA.passageTimes[selector])))
+    # DA.plot_passagetimedist(selector)
 
     DA.save_passage_times_in_ns_to_txt(selector, short + "_passagetimes_in_ns.txt")
 
     DA.calc_diffusion(selector)
     print(f"\t{short}-Diffusioncoefficient: " + str(DA.D[selector]).replace(".", ","))
-
     fig_diff = DA.plot_diffusion(selector)
     DA.save_fig_to_results(fig=fig_diff, name="diffusion_" + short)
+
+    DA.plot_starting_points("resname HEX and name C1")
+
+
+hexagonal_structure = HexagonalMembrane(
+    selector="resname C",
+    L=180,
+)
+
+cubic_structure = CubicMembrane(
+    selector="resname C",
+    cube_arrangement=(2, 2, 2),
+    cube_size=90,
+    pore_radius=15,
+)
+
+structure = None
+if cubic_or_hex == "C":
+    structure = cubic_structure
+elif cubic_or_hex == "H":
+    structure = hexagonal_structure
 
 
 # STEP 1: initialise the Data into the class
@@ -29,21 +52,22 @@ DA = DiffusionAnalysis(
     results_dir=path + "analysis/",
     analysis_max_step_size_ps=200,
     verbose=True,
-    L=180,
+    membrane=structure,
 )
 
 print(DA)
 
-DA.find_membrane_location_hexstructure(mem_selector="resname C")
-print("\tz_lower: " + str(DA.z_lower))
+DA.find_membrane_location()
+DA.print_membrane_location()
+DA.verify_membrane_location()
+plt.show()
 
-DA.verify_membrane_location(mem_selector="resname HEX and name C1")
 
 analyse_resname("resname HEX and name C1", "hex")
 
 analyse_resname("resname DOD and name C2", "dod")
 
 DA.store_results_json()
+print(DA)
 plt.show()
-print("\n\n\n RESULTS:")
-print(json.dumps(DA.D, indent=4))
+print("\n\n\n\n\n")
