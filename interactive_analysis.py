69d86ff65f269b17523d3bc9020f48bcf487bc66
Moritz Marquardt <moritz.g.marquardt@gmail.com>
2024-11-07 12:17:59 +0100

    feat: make it more interactive, enable solvent analysis

--- interactive_analysis.py
+++ interactive_analysis.py
@@ -8,7 +8,7 @@ from MembraneAnalysisToolbox.MembraneStructures import (
     Solvent,
 )
 
-path = input("Enter the path to the simulation: ")
+path = input("Enter the path to the simulation (the path has to end with '/'): ")
 print("Path: " + path + "\n")
 membrane_type = input(
     "Enter the type of membrane (cubic (C) or hexagonal (H) or solvent (S)): "
@@ -29,35 +29,39 @@ def analyse_resname(selector: str, short: str):
     fig_diff = DA.plot_diffusion(selector)
     DA.save_fig_to_results(fig=fig_diff, name="diffusion_" + short)
 
-    DA.plot_starting_points("resname HEX and name C1")
+    DA.plot_starting_points(selector)
 
-
-hexagonal_structure = HexagonalMembrane(
-    selector="resname C",
-    L=180,
-)
-
-cubic_structure = CubicMembrane(
-    selector="resname C",
-    cube_arrangement=(2, 2, 2),
-    cube_size=90,
-    pore_radius=15,
-)
-
-solvent_structure = Solvent(
-    lowerZ=None,
-    upperZ=None,
-)
 
 structure = None
 if membrane_type == "C":
-    structure = cubic_structure
+    structure = CubicMembrane(
+        selector="resname C",
+        cube_arrangement=(2, 2, 2),
+        cube_size=90,
+        pore_radius=15,
+    )
 elif membrane_type == "H":
-    structure = hexagonal_structure
+    L = input(
+        "Enter the length of the membrane in A or press enter to select the default of 180: "
+    )
+    if L == "":
+        L = 180
+    else:
+        L = int(L)
+    structure = HexagonalMembrane(
+        selector="resname C",
+        L=L,
+    )
 elif membrane_type == "S":
-    structure = solvent_structure
-    solvent_structure.lowerZ = input("Enter the lower Z value: ")
-    solvent_structure.upperZ = input("Enter the upper Z value: ")
+    lowerZ = int(input("Enter the lower Z value: "))
+    upperZ = int(input("Enter the upper Z value: "))
+    L = upperZ - lowerZ
+    structure = Solvent(
+        lowerZ=lowerZ,
+        upperZ=upperZ,
+        L=L,
+    )
+
 else:
     raise ValueError("Invalid input for membrane_type")
 
@@ -67,22 +71,40 @@ DA = DiffusionAnalysis(
     topology_file=path + "topol.tpr",
     trajectory_file=path + "traj.xtc",
     results_dir=path + "analysis/",
-    analysis_max_step_size_ps=200,
+    analysis_max_step_size_ps=20,
     verbose=True,
     membrane=structure,
 )
 
 print(DA)
 
-DA.find_membrane_location()
-DA.print_membrane_location()
-DA.verify_membrane_location()
+if isinstance(DA.membrane, Solvent):
+    DA.print_membrane_location()
+else:
+    DA.find_membrane_location()
+    DA.print_membrane_location()
+    DA.verify_membrane_location()
 plt.show()
 
+wants_to_analyse = True
+print("Analyse the transitions of atoms here:")
+while wants_to_analyse:
+    resname = input("Enter the resname (example: HEX): ")
+    name = input("Enter the name (example: C1): ")
+    selector = f"resname {resname} and name {name}"
+    short = resname.lower() + "_" + name.lower()
+    analyse_resname(selector, short)
 
-analyse_resname("resname HEX and name C1", "hex")
+    wants_to_analyse = (
+        input(
+            "\n\n\n Do you want to analyse the transitions of another resname? (y/n) "
+        )
+        == "y"
+    )
 
-analyse_resname("resname DOD and name C2", "dod")
+# analyse_resname("resname HEX and name C1", "hex")
+
+# analyse_resname("resname DOD and name R2", "dod")
 
 DA.store_results_json()
 print(DA)
